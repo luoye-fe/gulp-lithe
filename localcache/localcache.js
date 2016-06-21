4aa5a9e6288043b250874b46c4d48354f4e42740
zeromike <540187842@qq.com>
2016-06-21 16:28:58 +0800

    增加不使用localcache分支处理

--- localcache/localcache.js
+++ localcache/localcache.js
@@ -122,7 +122,7 @@ var main = function(config, lc) {
 		}
 		else {
 			var configContent = require('fs').readFileSync(path.join(config.basepath, lc.configFile))
-			fs.writeFileSync(path.join(lc.tmpPath, lc.configFile), configContent);
+			fs.writeFileSync(path.join(config.basepath + lc.tmpPath, lc.configFile), configContent);
 		}
 		callback();
 	}, function (callback){
